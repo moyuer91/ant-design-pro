3d9ca747515857876451fd7a673164535f0991a7
zhuangjinyang <qz.zjy@163.com>
2020-02-21 01:57:57 +0800

    1、添加附件上传图片压缩功能

--- src/pages/Visa/components/FileUpload.js
+++ src/pages/Visa/components/FileUpload.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Upload, Icon, Modal, message } from 'antd';
+import lrz from 'lrz';
 import { getToken } from '@/utils/authority';
 
 function getBase64(file) {
@@ -11,13 +12,82 @@ function getBase64(file) {
   });
 }
 
+function dataURLtoFile(dataurl, filename) {
+  const arr = dataurl.split(',');
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n > 0) {
+    n -= 1;
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  // 转换成file对象
+  return new File([u8arr], filename, { type: mime });
+  // 转换成成blob对象
+  // return new Blob([u8arr],{type:mime});
+}
+
+function backPromise(res) {
+  return new Promise(function(resolve, reject) {
+    if (res instanceof Object) {
+      // 将压缩后的base64字符串转换为文件对象
+      const file = dataURLtoFile(res.base64, res.origin.name);
+      // console.log('base64tofile',file);
+
+      // 需要给文件对象一个唯一的uid属性否则将会报错
+      Object.assign(file, { uid: file.lastModified });
+      resolve(file);
+      // console.log("压缩成功");
+    } else {
+      reject(new Error('压缩失败'));
+    }
+  });
+}
+
+// 处理函数
+function compress(file) {
+  try {
+    let ratio = 1;
+    const { size } = file;
+
+    // 图片大于1M就压缩，否则不压缩
+    if (size && size > 256000) {
+      // 将大于1M的图片，压缩后大致使图片大小保持在1M左右
+
+      ratio = `${256000 / size}`;
+
+      ratio = parseFloat(ratio).toFixed(2);
+
+      // console.log('开始压缩',ratio);
+      return lrz(file, { quality: ratio })
+        .then(rst => {
+          // 数字越小，压缩率越高
+          console.log(rst);
+          return backPromise(rst);
+        })
+        .catch(() => {
+          // console.log('压缩失败');
+          return false;
+        });
+    }
+  } catch (e) {
+    // console.log('压缩异常');
+    return false;
+  }
+  return true;
+}
+
 class FileUpload extends PureComponent {
   static defaultProps = {
     max: 1,
     value: [],
     listType: 'picture-card',
-    action: 'https://service.dameiweb.com/VISACENTER-PROGRAM/ossUpload/save',
+    action: 'https://service.dameiweb.com/VISACENTER-PROGRAM/ossUpload/save', // 可以自定义
     uploadBtnText: '上传照片',
+    compressPic: false, // 是否压缩图片
+    getUrl: null, // 调用方获取自定义接口返回值中的附件url，getUrl若为null，则用默认值
+    getThumbUrl: null, // 调用方获取自定义接口返回值中附件缩略图的url,不传则优先用getUrl，getUrl若为null，则用默认值
   };
 
   constructor(props) {
@@ -31,7 +101,7 @@ class FileUpload extends PureComponent {
   }
 
   handleChange = ({ fileList }) => {
-    const { onChange } = this.props;
+    const { onChange, getUrl, getThumbUrl } = this.props;
 
     // if (file.status==="error") {
     //   message.error("文件上传失败！");
@@ -40,10 +110,12 @@ class FileUpload extends PureComponent {
     const newValue = fileList
       .map(item => {
         if (item.status === 'done') {
+          // 如果自定义了返回值解析接口，则使用自定义解析接口，否则按默认格式解析
           return {
             ...item,
-            thumbUrl: item.response.data,
-            url: item.response.data,
+            url: getUrl ? getUrl(item) : item.response.data,
+            // eslint-disable-next-line
+            thumbUrl: getThumbUrl ? getThumbUrl(item) : getUrl ? getUrl(item) : item.response.data,
           };
         }
         if (item.status === 'error') {
@@ -73,17 +145,26 @@ class FileUpload extends PureComponent {
     });
   };
 
-  beforeUpload = () => {
-    // const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    // if (!isJpgOrPng) {
-    //   message.error('格式必须是JPG或PNG!');
-    // }
-    // const isLt2M = file.size / 1024 / 1024 < 200;
-    // if (!isLt2M) {
-    //   message.error('图片大小不能超过200MB!');
-    // }
-    // return isLt2M;
-    return true;
+  beforeUpload = file => {
+    const { compressPic } = this.props;
+    const isJpgOrPng =
+      file.type === 'image/jpeg' ||
+      file.type === 'image/png' ||
+      file.type === 'image/bmp' ||
+      file.type === 'image/jpg';
+    if (isJpgOrPng && compressPic) {
+      // console.log('file',file);
+      const p = compress(file);
+
+      // 官方文档 ：参数为上传的文件，函数若返回 false 则停止上传。
+      // 支持返回一个 Promise 对象，
+      // Promise 对象 reject 时则停止上传
+      // resolve 时开始上传（ resolve 传入 File 或 Blob 对象则上传 resolve 传入对象）。
+
+      // console.log(p);
+      return p;
+    }
+    return file;
   };
 
   render() {
@@ -108,6 +189,7 @@ class FileUpload extends PureComponent {
           }}
           onChange={this.handleChange}
           onPreview={this.handlePreview}
+          beforeUpload={this.beforeUpload}
         >
           {fileList && fileList.length >= max ? null : uploadButton}
         </Upload>
--- src/pages/Visa/components/PassportUpload.js
+++ src/pages/Visa/components/PassportUpload.js
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
 import { message, Spin } from 'antd';
 import moment from 'moment';
-import { getCheckedData, isSuccessful } from '@/utils/VisaUtils';
+import { getCheckedData } from '@/utils/VisaUtils';
 import FileUpload from './FileUpload';
-import request from '@/utils/request';
 
 const analyzePassportAction =
   'https://service.dameiweb.com/VISACENTER-PROGRAM/user/loadPassportInfo';
+
 class PassportUpload extends PureComponent {
   constructor(props) {
     super(props);
@@ -17,53 +17,42 @@ class PassportUpload extends PureComponent {
     };
   }
 
+  getUrl = item => {
+    return item.response.data.url;
+  };
+
   handleChange = fileList => {
     const { onChange, mapResultToForm } = this.props;
     const file = fileList[0];
     if (file && file.status === 'done') {
       // 解析时间较长，需要增加进度提示
-      this.setState({ loading: true });
+      // this.setState({ loading: true });
       // 上传完成后进行解析
-      const filedata = new FormData();
-      filedata.append('file', file.originFileObj);
-      request(analyzePassportAction, {
-        method: 'POST',
-        body: filedata,
-      }).then(parseResp => {
-        // message.success(`返回结果：${JSON.stringify(parseResp)}`,20);
-        try {
-          const checkData = JSON.parse(getCheckedData(parseResp));
-          if (checkData && isSuccessful(checkData)) {
-            checkData.birth_date = checkData.birth_date
-              ? moment(checkData.birth_date, 'YYYYMMDD')
-              : null;
-            checkData.issue_date = checkData.issue_date
-              ? moment(checkData.issue_date, 'YYYYMMDD')
-              : null;
-            checkData.expiry_date = checkData.expiry_date
-              ? moment(checkData.expiry_date, 'YYYYMMDD')
-              : null;
-            mapResultToForm(checkData);
-          } else {
-            message.error('无法识别护照，请手动录入信息');
-          }
-          // 去除解析进度条
-          this.setState({ loading: false });
-        } catch (e) {
+      try {
+        const { passportInfo } = getCheckedData(file.response);
+        const passportObj = JSON.parse(passportInfo);
+        if (passportObj) {
+          passportObj.birth_date = passportObj.birth_date
+            ? moment(passportObj.birth_date, 'YYYYMMDD')
+            : null;
+          passportObj.issue_date = passportObj.issue_date
+            ? moment(passportObj.issue_date, 'YYYYMMDD')
+            : null;
+          passportObj.expiry_date = passportObj.expiry_date
+            ? moment(passportObj.expiry_date, 'YYYYMMDD')
+            : null;
+          mapResultToForm(passportObj);
+        } else {
           message.error('无法识别护照，请手动录入信息');
-          // 去除解析进度条
-          this.setState({ loading: false });
         }
-      });
+        // 去除解析进度条
+        // this.setState({ loading: false });
+      } catch (e) {
+        message.error('无法识别护照，请手动录入信息');
+        // 去除解析进度条
+        // this.setState({ loading: false });
+      }
     }
-    // const newValue=fileList.map(item=>({
-    //   // uid:item.uid,
-    //   // name:item.name,
-    //   // status:item.status,
-    //   // response:item.response,
-    //   // linkProps:item.linkProps,
-    //   ...item,
-    // }));
 
     const newValue = [...fileList];
     if (onChange) {
@@ -79,10 +68,13 @@ class PassportUpload extends PureComponent {
         <Spin spinning={loading} tip="图片较大，护照信息解析需要一会儿,请稍候..." delay={500}>
           <FileUpload
             {...this.props}
+            action={analyzePassportAction}
             accept="image/png, image/jpeg , image/jpg , image/bmp"
             fileList={fileList || []}
             onChange={this.handleChange}
             uploadBtnText="上传护照"
+            getUrl={this.getUrl}
+            compressPic
           />
         </Spin>
       </div>
